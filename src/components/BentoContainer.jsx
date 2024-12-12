c8faa2e3ef089ff680f0a660bd7c0b805b2a5223
Dom <dominicfranz.stodomingo@gmail.com>
2024-12-12 10:30:59 +0800

    111224

--- src/components/BentoContainer.jsx
+++ src/components/BentoContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../styles/BentoContainer.css';
+import "../styles/BentoContainer.css";
 import TopLeftContainer from "./TopLeftContainer";
 import TopMiddleContainer from "./TopMiddleContainer";
 import TopRightContainer from "./TopRightContainer";
@@ -7,33 +7,32 @@ import Experiences from "./Experiences";
 import SkillCarousel from "./SkillCarousel";
 import getContacts from "../hooks/getContacts";
 import getSkills from "../hooks/getSkills";
-import getExperiences from "../hooks/getExperiences.js"
-
-
+import getExperiences from "../hooks/getExperiences.js";
 
 const BentoContainer = () => {
-    const contactsDetails = getContacts();
-    const skillsDetails = getSkills();
-    const experiencesDetails = getExperiences();
+  const contactsDetails = getContacts();
+  const skillsDetails = getSkills();
+  const experiencesDetails = getExperiences();
 
-    return(
-        <div className="bento-container h-auto flex flex-col gap-4">
-            <div className="bento-item left h-auto w-full flex flex-row gap-4">
-                <TopLeftContainer />
-                <TopMiddleContainer/>
-                <TopRightContainer contacts={contactsDetails}/>
-            </div>
-            <div className="bento-item  glass middle flex-auto h-auto w-full">
-                <SkillCarousel skills={skillsDetails}/>
-            </div>
-            <div className="bento-item flex h-full w-full gap-4">
-                <Experiences experiences={experiencesDetails}/>
-                <div className="glass h-2/5 w-full"></div>
-            </div>
-            <div className="bento-item flex-auto h-auto w-full"></div>
+  return (
+    <div className="bento-container h-auto flex flex-col gap-4">
+      <div className="bento-item left h-auto w-full flex flex-row gap-4">
+        <TopLeftContainer />
+        <TopMiddleContainer />
+        <TopRightContainer contacts={contactsDetails} />
+      </div>
+      <div className="bento-item glass middle flex-auto h-auto w-full">
+        <SkillCarousel skills={skillsDetails} />
+      </div>
+      <div className="bento-item flex w-full gap-4">
+        <div className="glass w-1/2">
+          <Experiences experiences={experiencesDetails} />
         </div>
-    );
-}
-
+        <div className="glass w-1/2 h-96"></div>
+      </div>
+      <div className="bento-item flex-auto h-auto w-full"></div>
+    </div>
+  );
+};
 
 export default BentoContainer;
--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Experience from "./Experience";
-import '../styles/Experiences.css';
+import "../styles/Experiences.css";
 
 const Experiences = ({ experiences }) => {
-    return(
-        <div className="experiences-container flex flex-col gap-2 p-2 glass w-full">
-            {experiences.map((experience) => (
-                <Experience key={experience.id} experience={experience}/>
-            ))}
+  return (
+    <div
+      className="experiences-container flex flex-col gap-2 p-2 w-full 
+                 overflow-y-auto snap-y snap-mandatory scroll-smooth h-96"
+    >
+      {experiences.map((experience) => (
+        <div key={experience.id} className="snap-start">
+          <Experience experience={experience} />
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default Experiences;
